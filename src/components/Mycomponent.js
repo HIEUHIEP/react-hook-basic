1730b7ed82ac3cd8bb1615527f092de9ec8b27ca
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-10-06 13:27:02 +0700

    test components

---
+++ src/components/Mycomponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Mycomponent extends React.Component {
+    // JSX
+    render() {
+        return (
+            <div>
+                Mycomponent first
+            </div>
+        );
+    }
+}
+
+export default Mycomponent;
