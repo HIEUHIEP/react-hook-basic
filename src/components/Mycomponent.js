95c3a98537460d3a5ded10acffdc2814014b09cd
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-10-07 20:11:55 +0700

    a

--- src/components/Mycomponent.js
+++ src/components/Mycomponent.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import UserInfor from './UserInfor'
+import UserInfor from './UserInfor';
+import DisplayInfor from './DisplayInfor';
 
 class Mycomponent extends React.Component {
 
 
     // JSX
     render() {
+        const myInfor = ['a', 'b', 'c']
+        const myAge = 30;
         return (
             <div>
                 <UserInfor />
+                <DisplayInfor name={"hieppu"} age={30} myInfor={myInfor} />
             </div>
         );
     }
