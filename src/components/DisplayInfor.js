195b578886cbd673a7e5eca261adc14b549d2dac
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-10-08 21:46:08 +0700

    useEffect

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,35 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './DisplayInfor.scss';
 import logo from './../logo.svg';
-
-// class DisplayInfor extends React.Component {
-
-//     render() {
-//         const { listUsers } = this.props;
-//         return (
-//             <div className='display-infor-container'>
-//                 {true &&
-//                     <>
-//                         {listUsers.map((user) => {
-//                             return (
-//                                 <div key={user.id} className={+user.age > 18 ? 'green' : 'red'}>
-//                                     <div>
-//                                         <div style={{ color: 'blue', paddingTop: '10px' }}>My name is {user.name}</div>
-//                                         <div>My age is {user.age}</div>
-//                                     </div>
-//                                     <div>
-//                                         <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete </button>
-//                                     </div>
-//                                     <hr />
-//                                 </div>
-//                             )
-//                         })}
-//                     </>
-//                 }
-//             </div >
-//         )
-//     }
-// }
 
 // function component
 // useState is a Hook
@@ -42,6 +13,14 @@ const DisplayInfor = (props) => {
     const handleShowHideListUser = () => {
         setShowHideListUser(!isShowHideListUser);
     }
+
+    useEffect(
+        () => {
+            if (listUsers.length === 0)
+                console.log('No users to display');
+        }, [listUsers] // tracking listUsers
+    );
+
     return (
         <div className='display-infor-container'>
             <div>
