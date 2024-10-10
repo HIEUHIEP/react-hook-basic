8bd03c7f90d12ca6fb347120478d9125cb8ffe6d
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-10-10 21:37:36 +0700

    design hompage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,23 @@
-const HomePage = () => {
+import VideoHomePage from '../../assets/video-homepage.mp4'
+
+const HomePage = (props) => {
     return (
         <div className="homepage-container">
-            home page component
+            <video autoPlay muted loop>
+                <source src={VideoHomePage} type="video/mp4">
+                </source>
+            </video>
+            <div className="homepage-content">
+                <div className="title-top">
+                    Get to know your customers with forms worth filling out
+                </div>
+                <div className="title-mid">
+                    Collect all the data you need to understand customers with forms designed to be refreshingly different.
+                </div>
+                <div className="title-bot">
+                    <button>Get started-it's free</button>
+                </div>
+            </div>
         </div>
     )
 }
